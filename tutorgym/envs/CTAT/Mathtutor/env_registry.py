81ff520b7a9a6d4bd36bef6317dee2865f579c6e
Danny Weitekamp <dannyweitekamp@gmail.com>
2025-02-14 18:04:38 -0500

    add env reg for Mathtutor

---
+++ tutorgym/envs/CTAT/Mathtutor/env_registry.py
@@ -0,0 +1,75 @@
+# Import all environment modules
+
+WORKING_DOMAINS = [
+    # Values [0]: Incomplete, value of expression matcher requires eval
+    # '6_01_HTML',
+
+    # Values [x]: Okay
+    '6_02_HTML',
+
+    # Values [0]: Complicated, mostly dynamic
+    # '6_05_HTML',
+
+    # Values [0]: Complicated, mostly dynamic (Highly custom)
+    # '6_06_HTML',
+
+    # Values [x]: Okay
+    '6_07_HTML',
+
+    # Values [0]:Complicated, mostly dynamic (Highly custom similar to 05) 
+    # '6_08_HTML',
+
+    # Values [x]: Okay
+    '6_09_HTML',
+
+    # Values [0]:Complicated, mostly dynamic (Highly custom similar to 05) 
+    # '6_10_HTML',
+
+    # Values [x]: Okay
+    '6_11_HTML',
+
+    # Values [0]: Expressions: ifThen, isVar, concat
+    '6_14_HTML',
+
+    # Values [x]: Only simple ExpressionMatches 
+    '6_15_HTML',
+
+    # Values [-]: Some simple ExpressionMatches, but several hard ones
+    # '6_16_HTML',
+
+    # Values [-]: Number line, seems feasible
+    # '6_17_HTML',
+
+    # Values [-]: Number line, seems feasible 
+    # '6_18_HTML',
+
+    # Values [-]: Number line, but seems feasible     
+    # '6_19_HTML',
+
+    # Values [-]: Number line, uses eval, but feasible 
+    # '6_20_HTML',
+
+    # Values [0]: Expressions, and(), rationalEquals, getDivisor
+    # '6_21_HTML',
+
+    # Values [x]: Just ExpressionMatches
+    # '6_24_HTML',
+
+    # Values [0]: Lots of regular expression matches, including regex in selection
+    # '6_25_HTML',
+
+    # Values [0]: Some regular expression matches, including regex in selection
+    # '6_26_HTML',
+
+    # Values [x]: Just ExpressionMatches
+    '6_27_HTML',
+
+    # Values [-]: Complicated Expressions, but not that complex
+    # '6_28_HTML',
+
+    # Values [x]: All simple values 
+    '6_30_HTML',
+    
+    # Values [x]: All simple values # 
+    '6_34_HTML',
+]
